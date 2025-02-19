de5e7869806178620866ba17323165d37f207bac
chaimamosbehi <mosbehi.chayma@gmail.com>
2025-02-19 21:54:41 +0100

    First commit

---
+++ src/test/java/com/e2eTest/automation/utils/Teardown.java
@@ -0,0 +1,29 @@
+package com.e2eTest.automation.utils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+
+public class Teardown {
+
+	/**
+	 * This method is used to close browser. This method is called after the
+	 * invocation of each test method in given class.
+	 * 
+	 * @After Methods annotated with @After execute after every scenario.
+	 */
+	@After
+	public void quitDriver(Scenario scenario) {
+
+		if (scenario.isFailed()) {
+			final byte[] screenshot = ((TakesScreenshot) Setup.getDriver()).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshot, "image/png", "screenshot");
+
+		}
+		Setup.getDriver().quit();
+		Setup.getLogger().info("Scenario: " + scenario.getName() + "Finished" + scenario.getStatus());
+
+	}
+
+}
---
+++ target/classes/connexion/01_Login.feature
@@ -0,0 +1,11 @@
+@login
+Feature: Login
+  En tant que utilisateur je souhaite tester la page de connexion
+
+  @login_valid_credential
+  Scenario: Login avec des données valides
+    Given Je me connecte sur l application Orange
+    When Je saisis le username "Admin
+    And Je saisis le mot de passe "admin123"
+    And Je clique sur le bouton Login
+    Then Redirection vers la page Dashboard "Dashboard"
